d0cfda52e95461ffb226264c3416952c82f1c67a
Mauricio Corte <Corte.Mauricio98@gmail.com>
2025-02-24 19:06:58 +0100

    Re-added files

---
+++ MinimalApiExercise/Data/MinimalApiExerciseContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MinimalApiExercise.Models;
+
+namespace MinimalApiExercise.Data;
+
+public class MinimalApiExerciseContext : DbContext
+{
+    public MinimalApiExerciseContext(DbContextOptions<MinimalApiExerciseContext> options) : base(options)
+    {
+        
+    }
+
+    public DbSet<Customer> Customers { get; set; }
+
+    public DbSet<Service> Services { get; set; }
+}
---
+++ MinimalApiExercise/Migrations/20250224160206_IncreasedPhoneNumberCharactersAllowed.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MinimalApiExercise.Migrations
+{
+    /// <inheritdoc />
+    public partial class IncreasedPhoneNumberCharactersAllowed : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "CustomerPhoneNumber",
+                table: "Customers",
+                type: "nvarchar(15)",
+                maxLength: 15,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(14)",
+                oldMaxLength: 14);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "CustomerPhoneNumber",
+                table: "Customers",
+                type: "nvarchar(14)",
+                maxLength: 14,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(15)",
+                oldMaxLength: 15);
+        }
+    }
+}
