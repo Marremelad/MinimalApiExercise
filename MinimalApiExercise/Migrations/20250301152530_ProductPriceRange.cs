fedc054b3fefb02a3d99e3d84d9df9b3a3739ff1
Mauricio Corte <Corte.Mauricio98@gmail.com>
2025-03-01 16:26:12 +0100

    Made sure that product price can only be a positive number

---
+++ MinimalApiExercise/Migrations/20250301152530_ProductPriceRange.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MinimalApiExercise.Migrations
+{
+    /// <inheritdoc />
+    public partial class ProductPriceRange : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Products",
+                type: "nvarchar(100)",
+                maxLength: 100,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(35)",
+                oldMaxLength: 35);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Products",
+                type: "nvarchar(35)",
+                maxLength: 35,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(100)",
+                oldMaxLength: 100);
+        }
+    }
+}
--- MinimalApiExercise/Models/Product.cs
+++ MinimalApiExercise/Models/Product.cs
@@ -7,13 +7,15 @@ namespace MinimalApiExercise.Models
         [Key]
         public int Id { get; set; }
 
-        [StringLength(35)]
+        [StringLength(100)]
         public required string Name { get; set; }
         
         [StringLength(300)]
         public string? Description { get; set; }
 
+        [Range(0, double.MaxValue, ErrorMessage = "Price must be a positive number.")]
         public decimal Price { get; set; }
+
         
         public virtual List<OrderProduct>? OrderProducts { get; set; }
     }
