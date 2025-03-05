a1155b71a96843fcd3baf6bc8200ff792ddda60b
Mauricio Corte <Corte.Mauricio98@gmail.com>
2025-03-05 14:45:46 +0100

    Restructured endpoints and services for Products

--- MinimalApiExercise/Endpoints/ProductEndpoints.cs
+++ MinimalApiExercise/Endpoints/ProductEndpoints.cs
@@ -4,22 +4,62 @@ namespace MinimalApiExercise.Endpoints;
 
 public abstract class ProductEndpoints
 {
+    private const string InvalidOperationMessage = "Invalid operation status";
+    
     public static void RegisterEndpoints(WebApplication app)
     {
         // Get all products.
-        app.MapGet("/products", async (ProductService productService) => 
-            await productService.GetAllProducts()); 
+        app.MapGet("/products", async (ProductService productService) =>
+        {
+            var (operationStatus, response) = await productService.GetAllProducts();
+
+            return operationStatus switch
+            {
+                0 => Results.Ok(response),
+                1 => Results.Problem(response.ToString(), statusCode: 500),
+                _ => Results.BadRequest()
+            };
+        });
         
         // Get product.
-        app.MapGet("/products/{id:int}", async (ProductService productService, int id) => 
-            await productService.GetProduct(id));
+        app.MapGet("/products/{id:int}", async (ProductService productService, int id) =>
+        {
+            var (operationStatus, response) = await productService.GetProduct(id);
+
+            return operationStatus switch
+            {
+                0 => Results.Ok(response),
+                1 => Results.Problem(response.ToString(), statusCode: 500),
+                2 => Results.NotFound(response),
+                _ => Results.BadRequest(InvalidOperationMessage)
+            };
+        });
         
         // Search for products.
-        app.MapGet("products/search", async (ProductService productService, string? query) => 
-            await productService.SearchProducts(query));
+        app.MapGet("products/search", async (ProductService productService, string? query) =>
+        {
+            var (operationStatus, response) = await productService.SearchProducts(query);
+
+            return operationStatus switch
+            {
+                0 => Results.Ok(response),
+                1 => Results.Problem(response.ToString(), statusCode: 500),
+                _ => Results.BadRequest(InvalidOperationMessage)
+            };
+        });
         
         // Search for product by price range.
-        app.MapGet("products/priceRange/search", async (ProductService productService, decimal? minPrice, decimal? maxPrice) => 
-            await productService.SearchProductByPriceRange(minPrice, maxPrice));
+        app.MapGet("products/priceRange/search",
+            async (ProductService productService, decimal? minPrice, decimal? maxPrice) =>
+            {
+                var (operationStatus, response) = await productService.SearchProductByPriceRange(minPrice, maxPrice);
+
+                return operationStatus switch
+                {
+                    0 => Results.Ok(response),
+                    1 => Results.Problem(response.ToString(), statusCode: 500),
+                    _ => Results.BadRequest(InvalidOperationMessage)
+                };
+            });
     }
 }
--- MinimalApiExercise/Services/ProductService.cs
+++ MinimalApiExercise/Services/ProductService.cs
@@ -6,73 +6,113 @@ namespace MinimalApiExercise.Services;
 
 public class ProductService(StoreDbContext context)
 {
+    private const string OperationsSuccessfulMessage = "Operation successful";
+    private const string NotFoundMessage = "not found";
+    private const string OrderTerminatedMessage = "Order terminated:";
+    
     // Get all products.
-    public async Task<IResult> GetAllProducts()
+    public async Task<(int, object)> GetAllProducts()
     {
-        return Results.Ok(await context.Products
-            .Select(p => new ProductDto
-            {
-                ProductId = p.Id,
-                ProductName = p.Name,
-                ProductDescription = p.Description,
-                ProductPrice = p.Price
-            })
-            .ToListAsync());
+        List<ProductDto> products;
+        try
+        {
+            products = await context.Products
+                .Select(p => new ProductDto
+                {
+                    ProductId = p.Id,
+                    ProductName = p.Name,
+                    ProductDescription = p.Description,
+                    ProductPrice = p.Price
+                })
+                .ToListAsync();
+        }
+        catch (Exception e)
+        {
+            return (1, e);
+        }
+
+        return (0, products);
     }
     
     // Get product.
-    public async Task<IResult> GetProduct(int id)
+    public async Task<(int, object)> GetProduct(int id)
     {
-        var product = await context.Products
-            .Where(p => p.Id == id)
-            .Select(p => new ProductDto
-            {
-                ProductId = p.Id,
-                ProductName = p.Name,
-                ProductDescription = p.Description,
-                ProductPrice = p.Price
-            })
-            .FirstOrDefaultAsync();
+        ProductDto? product;
+        try
+        {
+            product = await context.Products
+                .Where(p => p.Id == id)
+                .Select(p => new ProductDto
+                {
+                    ProductId = p.Id,
+                    ProductName = p.Name,
+                    ProductDescription = p.Description,
+                    ProductPrice = p.Price
+                })
+                .FirstOrDefaultAsync();
 
-        return product == null ? Results.NotFound("Product does not exist") : Results.Ok(product);
+            if (product == null) return (2, "Product " + NotFoundMessage);
+        }
+        catch (Exception e)
+        {
+            return (1, e);
+        }
+
+        return (0, product);
     }
     
     // Search products.
-    public async Task<IResult> SearchProducts(string? query)
+    public async Task<(int, object)> SearchProducts(string? query)
     {
-        var products = await context.Products
-            .Select(p => new ProductDto
-            {
-                ProductId = p.Id,
-                ProductName = p.Name,
-                ProductDescription = p.Description,
-                ProductPrice = p.Price
-            })
-            .ToListAsync();
-
+        List<ProductDto> products;
+        try
+        {
+            products = await context.Products
+                .Select(p => new ProductDto
+                {
+                    ProductId = p.Id,
+                    ProductName = p.Name,
+                    ProductDescription = p.Description,
+                    ProductPrice = p.Price
+                })
+                .ToListAsync();
+        }
+        catch (Exception e)
+        {
+            return (1, e);
+        }
+        
         return string.IsNullOrEmpty(query)
-            ? Results.Ok(products)
-            : Results.Ok(products.Where(p => p.ProductName
+            ? (0, products)
+            : (0, products.Where(p => p.ProductName
                 .Contains(query, StringComparison.CurrentCultureIgnoreCase)).ToList());
     }
     
     // Search products by price range.
-    public async Task<IResult> SearchProductByPriceRange(decimal? minPrice, decimal? maxPrice)
+    public async Task<(int, object)> SearchProductByPriceRange(decimal? minPrice, decimal? maxPrice)
     {
-        var products = await context.Products
-            .Where(p => 
-                (!minPrice.HasValue || p.Price >= minPrice.Value) && 
-                (!maxPrice.HasValue || p.Price <= maxPrice.Value)
-            )
-            .Select(p => new ProductDto
-            {
-                ProductId = p.Id,
-                ProductName = p.Name,
-                ProductDescription = p.Description,
-                ProductPrice = p.Price
-            })
-            .ToListAsync();
+        List<ProductDto> products;
+        try
+        {
+            products = await context.Products
+                .Where(p => 
+                    (!minPrice.HasValue || p.Price >= minPrice.Value) && 
+                    (!maxPrice.HasValue || p.Price <= maxPrice.Value)
+                )
+                .Select(p => new ProductDto
+                {
+                    ProductId = p.Id,
+                    ProductName = p.Name,
+                    ProductDescription = p.Description,
+                    ProductPrice = p.Price
+                })
+                .ToListAsync();
+        }
+        catch (Exception e)
+        {
+            return (1, e);
+        }
 
-        return Results.Ok(products);
+        return (0, products);
     }
 }
